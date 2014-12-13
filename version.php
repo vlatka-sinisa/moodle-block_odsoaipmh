73a8da55d88f6b9f06ab2bf851b1e55166dec3c0
vlatka-sinisa <st35634>
2014-12-13 23:18:22 +0100

    Fix: version number

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014060401;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014121301;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2010112400;        // Requires this Moodle version
 $plugin->component = 'block_odsoaipmh'; // Full name of the plugin (used for diagnostics)
 $plugin->release   = 1.2;		// Release version
